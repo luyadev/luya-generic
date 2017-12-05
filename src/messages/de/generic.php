3c0f3883df08ac3fed5e353ea999854ff2a7a613
Martin Petrasch <martin.petrasch@zephir.ch>
2017-12-05 17:20:28 +0100

    fix german messages

--- src/messages/de/generic.php
+++ src/messages/de/generic.php
@@ -7,20 +7,20 @@ return [
     'block_file_list_showtype_yes' => "Ja",
     'block_file_list_showtype_no' => "Nein",
     'block_line_name' => 'Linie (Trennung)',
-    'block_line_linespace_label' => 'Linien Abstand',
-    'block_line_linespace_space' => 'Abstand(Oben/Unten)',
-    'block_line_linestyle_label' => 'Linien Style',
+    'block_line_linespace_label' => 'Linienabstand',
+    'block_line_linespace_space' => 'Abstand (Oben/Unten)',
+    'block_line_linestyle_label' => 'Linienstyle',
     'block_line_linestyle_dotted' => 'Gepunktet',
     'block_line_linestyle_dashed' => 'Gestrichelt',
     'block_line_linestyle_solid' => 'Durchgängig',
-    'block_line_linewidth_label' => 'Linien Breite',
-    'block_line_linecolor_label' => 'Linien Farbe',
+    'block_line_linewidth_label' => 'Linienbreite',
+    'block_line_linecolor_label' => 'Linienfarbe',
     'block_list_name' => 'Auflistung',
     'block_list_elements_label' => 'Elemente',
     'block_list_listtype_label' => 'Typ',
     'block_list_listtype_ul' => 'Auflistung',
     'block_list_listtype_ol' => 'Aufzählung',
-    'block_list_no_content' => 'Noch kein Inhalt.',
+    'block_list_no_content' => 'Noch kein Inhalt hinterlegt.',
     'block_quote_name' => 'Zitat',
     'block_quote_content_label' => 'Text',
     'block_quote_no_content' => 'Es wurde noch kein Zitat eingegeben.',
@@ -37,13 +37,13 @@ return [
     'block_title_no_content' => 'Es wurde noch keine Überschrift eingegeben.',
     'block_wysiwyg_name' => 'Texteditor',
     'block_wysiwyg_content_label' => 'Inhalt',
-    'block_wysiwyg_help_content' => 'Klicken Sie in die erste Zeile um mit der Eingabe zu beginnen.',
+    'block_wysiwyg_help_content' => 'Klicken sie in die erste Zeile um mit der Eingabe zu beginnen.',
     'block_wysiwyg_no_content' => 'Es wurde noch kein Text eingegeben.',
     'block_line_linecolor_grey' => 'Grau',
     'block_line_linecolor_black' => 'Schwarz',
     
     // rc1
-    'block_cfg_additonal_css_class' => 'CSS Klasse', // this is not block related and can be ussed in all blocks.
+    'block_cfg_additonal_css_class' => 'CSS-Klasse', // this is not block related and can be ussed in all blocks.
     
 	// rc3
     'block_file_list_admin_empty' => 'Es wurden noch keine Dateien ausgewählt.',
