f2ee705593b1d115a7089c61c790d5d48779e2da
Basil Suter <git@nadar.io>
2017-12-18 10:58:47 +0100

    Wrap markdown text with a div if a css class is provided. closes #3

--- src/views/blocks/TextBlock.php
+++ src/views/blocks/TextBlock.php
@@ -2,9 +2,12 @@
 /**
  * @var $this \luya\cms\base\PhpBlockView
 */
+$cssClass = $this->cfgValue('cssClass');
 ?>
 <?php if ($this->varValue('textType') == 0): ?>
 <p<?= $this->cfgValue('cssClass', null, ' class="{{cssClass}}"'); ?>><?= $this->extraValue('text'); ?></p>
 <?php else: ?>
+<?php if ($cssClass): ?><div class="<?= $cssClass; ?>"><?php endif; ?>
 <?= $this->extraValue('text'); ?>
+<?php if ($cssClass): ?></div><?php endif; ?>
 <?php endif; ?>
