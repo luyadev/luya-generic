96c9fb122c01afd4bfcdc348e11b4ebe05794b5f
Iskender TOTOĞLU <iskender@altivebir.com>
2018-06-14 20:11:43 +0300

    Turkish messages (#8)

---
+++ src/messages/tr/generic.php
@@ -0,0 +1,50 @@
+<?php
+
+return [
+    'block_file_list_name' => 'Dosya listesi',
+    'block_file_list_files_label' => 'Dosyalar',
+    'block_file_list_files_showtype_label' => 'Dosya tipini göster?',
+    'block_file_list_showtype_yes' => "Evet",
+    'block_file_list_showtype_no' => "Hayır",
+    'block_line_name' => 'Ayraç',
+    'block_line_linespace_label' => 'Boşluk',
+    'block_line_linespace_space' => 'Boşluk (üst/alt)',
+    'block_line_linestyle_label' => 'Stil',
+    'block_line_linestyle_dotted' => 'Noktalı',
+    'block_line_linestyle_dashed' => 'Kesik',
+    'block_line_linestyle_solid' => 'Sürekli',
+    'block_line_linewidth_label' => 'Kalınlık',
+    'block_line_linecolor_label' => 'Renk',
+    'block_list_name' => 'Liste',
+    'block_list_elements_label' => 'Öğeler',
+    'block_list_listtype_label' => 'Tiğ',
+    'block_list_listtype_ul' => 'Listeleme',
+    'block_list_listtype_ol' => 'Numaralandırma',
+	'block_list_no_content' => 'Henüz içerik yok.',
+    'block_quote_name' => 'Alıntı',
+    'block_quote_content_label' => 'Metin',
+    'block_quote_no_content' => 'Henüz bir alıntı bilgisi girilmedi.',
+    'block_text_name' => 'Metin',
+    'block_text_content_label' => 'Metin',
+    'block_text_texttype_label' =>'Metin tipi',
+    'block_text_texttype_normal' => 'Normal metin',
+    'block_text_texttype_markdown' => 'Markdown Metni',
+    'block_text_no_content' => 'Henüz bir metin girilmedi.',
+    'block_title_name' => 'Başlık',
+    'block_title_content_label' => 'Başlık',
+    'block_title_headingtype_label' => 'Boyut',
+    'block_title_headingtype_heading' => 'Başlık',
+    'block_title_no_content' => 'Henüz bir başlık bilgisi girilmedi.',
+    'block_wysiwyg_name' => 'Metin Editörü',
+    'block_wysiwyg_content_label' => 'İçerik',
+    'block_wysiwyg_help_content' => 'Veri eklemek için ilk satıra tıklayın',
+    'block_wysiwyg_no_content' => 'Henüz bir metin girilmedi.',
+    'block_line_linecolor_grey' => 'Gri',
+    'block_line_linecolor_black' => 'Siyah',
+    
+    // rc1
+    'block_cfg_additonal_css_class' => 'CSS sınıfları', // this is not block related and can be ussed in all blocks.
+   
+    // rc3
+    'block_file_list_admin_empty' => 'Henüz bir dosya deçilmedi.',
+];
