c575b1680839d2227ddb7b2f9bdb7f4bbc984c0c
vave <fiolekcs@o2.pl>
2018-08-25 10:38:58 +0200

    Polish translation (#10)
    * Create generic.php
    
    * Update generic.php

---
+++ src/messages/pl/generic.php
@@ -0,0 +1,50 @@
+<?php
+
+return [
+    'block_file_list_name' => 'Lista plików',
+    'block_file_list_files_label' => 'Pliki',
+    'block_file_list_files_showtype_label' => 'Wyświetlaj typ pliku?',
+    'block_file_list_showtype_yes' => 'Tak',
+    'block_file_list_showtype_no' => 'Nie',
+    'block_line_name' => 'Linia podziału',
+    'block_line_linespace_label' => 'Odstępy',
+    'block_line_linespace_space' => 'Odstęp (góra / dół)',
+    'block_line_linestyle_label' => 'Styl',
+    'block_line_linestyle_dotted' => 'Dotted',
+    'block_line_linestyle_dashed' => 'Dashed',
+    'block_line_linestyle_solid' => 'Ciągły',
+    'block_line_linewidth_label' => 'Grubość',
+    'block_line_linecolor_label' => 'Kolor',
+    'block_list_name' => 'Lista',
+    'block_list_elements_label' => 'Elements',
+    'block_list_listtype_label' => 'Typ',
+    'block_list_listtype_ul' => 'Lista',
+    'block_list_listtype_ol' => 'Numeracja',
+    'block_list_no_content' => 'Brak treści.',
+    'block_quote_name' => 'Cytat',
+    'block_quote_content_label' => 'Tekst',
+    'block_quote_no_content' => 'Żaden cytat nie został jeszcze dostarczony.',
+    'block_text_name' => 'Tekst',
+    'block_text_content_label' => 'Tekst',
+    'block_text_texttype_label' => 'Typ tekstu',
+    'block_text_texttype_normal' => 'Normalny tekst',
+    'block_text_texttype_markdown' => 'Tekst oznaczenia',
+    'block_text_no_content' => 'Nie podano jeszcze tekstu.',
+    'block_title_name' => 'Nagłówek',
+    'block_title_content_label' => 'Tytuł',
+    'block_title_headingtype_label' => 'Rozmiar',
+    'block_title_headingtype_heading' => 'Heading',
+    'block_title_no_content' => 'Nie podano jeszcze danych dotyczących kursu.',
+    'block_wysiwyg_name' => 'Texteditor',
+    'block_wysiwyg_content_label' => 'Treść',
+    'block_wysiwyg_help_content' => 'Kliknij pierwszy wiersz, aby dodać dane ',
+    'block_wysiwyg_no_content' => 'Nie podano jeszcze tekstu.',
+    'block_line_linecolor_grey' => 'Szary',
+    'block_line_linecolor_black' => 'Czarny',
+
+    // rc1
+    'block_cfg_additonal_css_class' => 'Klasy CSS', // to nie jest związane z blokami i może być używane we wszystkich blokach.
+
+    // rc3
+    'block_file_list_admin_empty' => 'Nie wybrano jeszcze żadnego pliku.',
+];
