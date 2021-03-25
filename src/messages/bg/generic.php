32df4f126a0285549ff646091d744b157e41bab1
testt23 <rekstomi@abv.bg>
2021-03-25 16:14:56 +0200

    generic.php
    Translate for Bulgarian

---
+++ src/messages/bg/generic.php
@@ -0,0 +1,54 @@
+<?php
+
+return [
+    'block_file_list_name' => 'Списък с файлове',
+    'block_file_list_files_label' => 'Файлове',
+    'block_file_list_files_showtype_label' => 'Показване на типа файл?',
+    'block_file_list_showtype_yes' => "Да",
+    'block_file_list_showtype_no' => "Не",
+    'block_line_name' => 'Разделителна линия',
+    'block_line_linespace_label' => 'Разстояние',
+    'block_line_linespace_space' => 'Разстояние (отгоре / отдолу)',
+    'block_line_linestyle_label' => 'Стил',
+    'block_line_linestyle_dotted' => 'На точки',
+    'block_line_linestyle_dashed' => 'Пунктирано',
+    'block_line_linestyle_solid' => 'Непрекъснато',
+    'block_line_linewidth_label' => 'Дебелина',
+    'block_line_linecolor_label' => 'Цвят',
+    'block_list_name' => 'Списък',
+    'block_list_elements_label' => 'Елементи',
+    'block_list_listtype_label' => 'Тип',
+    'block_list_listtype_ul' => 'Обява',
+    'block_list_listtype_ol' => 'Номериране',
+    'block_list_no_content' => 'Все още няма съдържание.',
+    'block_quote_name' => 'Цитат',
+    'block_quote_content_label' => 'Текст',
+    'block_quote_no_content' => 'Все още не е предоставена оферта.',
+    'block_text_name' => 'Текст',
+    'block_text_content_label' => 'Текст',
+    'block_text_texttype_label' => 'Тип на текста',
+    'block_text_texttype_normal' => 'Нормален текст',
+    'block_text_texttype_markdown' => 'Текст за маркиране',
+    'block_text_no_content' => 'Все още не е предоставен текст.',
+    'block_title_name' => 'Заглавие',
+    'block_title_content_label' => 'Заглавие',
+    'block_title_headingtype_label' => 'Размер',
+    'block_title_headingtype_heading' => 'Заглавие',
+    'block_title_no_content' => 'Все още не са предоставени данни за заглавията.',
+    'block_wysiwyg_name' => 'Texteditor',
+    'block_wysiwyg_content_label' => 'Съдържание',
+    'block_wysiwyg_help_content' => 'Щракнете върху първия ред, за да добавите данни',
+    'block_wysiwyg_no_content' => 'Все още не е предоставен текст.',
+    'block_line_linecolor_grey' => 'Сиво',
+    'block_line_linecolor_black' => 'Черен',
+    
+    // rc1
+    'block_cfg_additonal_css_class' => 'CSS класове', // това не е свързано с блок и може да се използва във всички блокове.
+   
+    // rc3
+    'block_file_list_admin_empty' => 'Все още не е избран файл.',
+
+    // 1.0.3
+    'block_cfg_list_css_class' => 'Списък css клас',
+    'block_cfg_element_css_class' => 'Елемент css клас',
+];
