626567d2c00370c2a0bc75fd51ee8db6dd1d6d5a
dennisgon <dennisgon69@hotmail.com>
2021-10-02 04:27:06 +0700

    add translation in indonesia languange

---
+++ src/messages/id/generic.php
@@ -0,0 +1,54 @@
+<?php
+
+return [
+    'block_file_list_name' => 'Daftar File',
+    'block_file_list_files_label' => 'File',
+    'block_file_list_files_showtype_label' => 'Tampilkan tipe file?',
+    'block_file_list_showtype_yes' => "Ya",
+    'block_file_list_showtype_no' => "Tidak",
+    'block_line_name' => 'Garis pemisah',
+    'block_line_linespace_label' => 'Spasi',
+    'block_line_linespace_space' => 'Spasi (atas/bawah)',
+    'block_line_linestyle_label' => 'Style',
+    'block_line_linestyle_dotted' => 'titik-titik',
+    'block_line_linestyle_dashed' => 'garis-garis',
+    'block_line_linestyle_solid' => 'Berkelanjutan',
+    'block_line_linewidth_label' => 'Ketebalan',
+    'block_line_linecolor_label' => 'Warna',
+    'block_list_name' => 'Daftar',
+    'block_list_elements_label' => 'Elemen',
+    'block_list_listtype_label' => 'Jenis',
+    'block_list_listtype_ul' => 'Daftar',
+    'block_list_listtype_ol' => 'Penomoran',
+    'block_list_no_content' => 'Belum ada konten.',
+    'block_quote_name' => 'Kutipan',
+    'block_quote_content_label' => 'Teks',
+    'block_quote_no_content' => 'Belum ada penawaran.',
+    'block_text_name' => 'Teks',
+    'block_text_content_label' => 'Teks',
+    'block_text_texttype_label' =>'Jenis teks',
+    'block_text_texttype_normal' => 'Teks Normal',
+    'block_text_texttype_markdown' => 'Teks Penurunan Harga',
+    'block_text_no_content' => 'Belum ada teks yang disediakan.',
+    'block_title_name' => 'Judul',
+    'block_title_content_label' => 'Judul',
+    'block_title_headingtype_label' => 'Ukuran',
+    'block_title_headingtype_heading' => 'Judul',
+    'block_title_no_content' => 'Data heading belum tersedia.',
+    'block_wysiwyg_name' => 'Editor teks',
+    'block_wysiwyg_content_label' => 'Konten',
+    'block_wysiwyg_help_content' => 'Klik baris pertama untuk menambahkan data',
+    'block_wysiwyg_no_content' => 'Belum ada teks yang diberikan.',
+    'block_line_linecolor_grey' => 'Abu-abu',
+    'block_line_linecolor_black' => 'Hitam',
+    
+    // rc1
+    'block_cfg_additonal_css_class' => 'Kelas CSS', // ini tidak terkait dengan blok dan dapat digunakan di semua blok.
+   
+    // rc3
+    'block_file_list_admin_empty' => 'Belum ada file yang dipilih.',
+
+    // 1.0.3
+    'block_cfg_list_css_class' => 'Daftar kelas css',
+    'block_cfg_element_css_class' => 'Kelas elemen css',
+];
